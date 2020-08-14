7b6c7fd6a49078fd7edb3c0b467fb618ba0b560e
Dyony <dyony_rezende@live.com>
2020-08-13 21:56:14 -0300

    just creating a layout file

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<head>
+    <title>My Portfolio</title>
+    <meta http-equiv="Content-Language" content="pt-br">
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="{{URL::asset('css/app.css')}}">
+
+</head>
+
+
+<body>
+
+<main>
+    <button type="button" class="btn btn-success">Success</button>
+</main>
+
+
+<footer>
+
+</footer>
+
+</body>
+</html>
